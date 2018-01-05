7c6f354114f9f4dfceb57d4f29945e5d9e8f75b8
pgngp <pankajvedg@gmail.com>
2018-01-04 19:57:14 -0800

    Added multiple send/receive messages between client and server.

--- network_programming/client.c
+++ network_programming/client.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
 int main()
 {
-    char data[100] = "Hello World!";
-    char buffer[1000] = {0};
+    char data[100] = {};
+    char buffer[1000] = {};
 
     // Create a socket
 		int client_socket;
@@ -30,10 +30,22 @@ int main()
 		}
 
 		// Send request
-		send(client_socket, buffer, sizeof(buffer), 0);
+		strcpy(data, "send me 'hello'");
+		send(client_socket, data, sizeof(data), 0);
 		printf("Sent request...\n");
+
+		// Receive response
 		recv(client_socket, &buffer, sizeof(buffer), 0);
 		printf("Server response: %s\n", buffer);
+
+		// Send 2nd request
+		strcpy(data, "send me 'world'");
+		send(client_socket, data, sizeof(data), 0);
+		printf("Sent 2nd request...\n");
+
+		// Receive 2nd response
+		recv(client_socket, &buffer, sizeof(buffer), 0);
+		printf("Server 2nd response: %s\n", buffer);
 
 		// Close socket
 		// close(client_socket);
--- network_programming/server.c
+++ network_programming/server.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 
@@ -9,45 +9,59 @@
 int main()
 {
 	char server_message[256] = "Hi, this is the server";
-	
+	char buffer[1000] = {};
+
 	// Create server socket
 	int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_socket < 0) {
 		perror("Error: Could not create a socket\n");
 		exit(EXIT_FAILURE);
 	}
-	
+
 	// Define server address
 	struct sockaddr_in server_address;
 	server_address.sin_family = AF_INET;
 	server_address.sin_port = htons(10001);
 	server_address.sin_addr.s_addr = INADDR_ANY;
-	
+
 	// Bind socket to IP address and port
 	if (bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address)) < 0) {
 		perror("Error: Could not bind socket\n");
 		exit(EXIT_FAILURE);
 	}
-	
+
 	// Listen for connections
 	if (listen(server_socket, 10) < 0) {
 		perror("Error: Could not listen for connections\n");
 		exit(EXIT_FAILURE);
 	}
 	printf("Listening...\n");
-	
+
 	int client_socket;
 	if ((client_socket = accept(server_socket, NULL, NULL)) < 0) {
 		perror("Error: Could not accept connection\n");
 		exit(EXIT_FAILURE);
 	}
 	printf("Client connection accepted...\n");
-	
+
+	// Receive message from client
+	recv(client_socket, &buffer, sizeof(buffer), 0);
+	printf("Client message: %s\n", buffer);
+
 	// Send message
+	strcpy(server_message, "hello");
 	send(client_socket, server_message, sizeof(server_message), 0);
-	
+
+	// Receive 2nd message from client
+	recv(client_socket, &buffer, sizeof(buffer), 0);
+	printf("Client 2nd message: %s\n", buffer);
+
+	// Send 2nd message
+	strcpy(server_message, "world");
+	send(client_socket, server_message, sizeof(server_message), 0);
+
 	// Close socket
 	// close(server_socket);
-	
+
 	return 0;	
 }
