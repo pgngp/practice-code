18a343a40df1763f4d0bb001fd4a165e8e8127c4
Pankaj Gupta <pankaj.gupta@spoken.com>
2019-11-07 16:43:50 -0800

    another accepted version

--- stringsArrays/WordLadder.java
+++ stringsArrays/WordLadder.java
@@ -27,19 +27,128 @@ public class WordLadder {
         if (!notVisited.contains(endWord)) {
             return 0;
         }
+
+        List<String> visitedList = new ArrayList<>();
+        visitedList.add(endWord);
+        notVisited.remove(endWord);
+
+        char[] letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+        int result = helper(beginWord, notVisited, letters, 1, visitedList, 0);
+
+        return result == Integer.MAX_VALUE ? 0 : result;
+    }
+
+    private int helper(String beginWord, Set<String> notVisited, char[] letters, int distance, List<String> visitedList, int startIdx) {
+        int visitedListSize = visitedList.size();
+        if (startIdx >= visitedListSize) {
+            return Integer.MAX_VALUE;
+        }
+
+        for (int i = startIdx; i < visitedListSize; ++i) {
+            String word = visitedList.get(i);
+            char[] charArr = word.toCharArray();
+            for (int pos = 0; pos < word.length(); ++pos) {
+                char orig = charArr[pos];
+                for (char c : letters) {
+                    charArr[pos] = c;
+                    String newWord = new String(charArr);
+                    if (newWord.equals(beginWord)) {
+                        return distance + 1;
+                    } else if (!notVisited.contains(newWord)) {
+                        continue;
+                    }
+                    notVisited.remove(newWord);
+                    visitedList.add(newWord);
+                }
+                charArr[pos] = orig;
+            }
+        }
+
+        return helper(beginWord, notVisited, letters, distance + 1, visitedList, visitedListSize);
+    }
+
+    public int ladderLength2(String beginWord, String endWord, List<String> wordList) {
+        Set<String> notVisited = new HashSet<>();
+        for (String w : wordList) {
+            notVisited.add(w);
+        }
+
+        if (!notVisited.contains(endWord)) {
+            return 0;
+        }
         notVisited.remove(endWord);
         
         Set<String> visited = new HashSet<>();
         visited.add(endWord);
         
         char[] letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+        Map<String, Integer> cache = new HashMap<>();
+        cache.put(endWord, 1);
+        cache.put(beginWord, Integer.MAX_VALUE);
         
-        int result = 1 + helper(beginWord, visited, notVisited, letters, 1);
-        
-        return result;
+        //int result = 1 + helper5(beginWord, visited, notVisited, letters, 1);
+        //return result;
+        notVisited.remove(beginWord);
+        helper2(beginWord, visited, notVisited, letters, 1, cache);
+        return cache.get(beginWord) == Integer.MAX_VALUE ? 0 : cache.get(beginWord);
     }
 
-    private int helper(String beginWord, Set<String> visited, Set<String> notVisited, char[] letters, int distance) {
+    private void helper2(String beginWord, Set<String> visited, Set<String> notVisited, char[] letters, int distance, Map<String, Integer> cache) {
+        Set<String> newSet = new HashSet<>();
+        Iterator iter = notVisited.iterator();
+        int setSize = notVisited.size();
+        while (iter.hasNext()) {
+            String word = (String) iter.next();
+            boolean transformable = isTransformable(word, beginWord);
+            char[] arr = word.toCharArray();
+            for (int i = 0; i < arr.length; ++i) {
+                char orig = arr[i];
+                for (char c : letters) {
+                    if (c == orig) {
+                        continue;
+                    }
+                    arr[i] = c;
+                    String newWord = new String(arr);
+                    if (!cache.containsKey(newWord) || newWord.equals(beginWord)) {
+                        continue;
+                    }
+                    if (transformable) {
+                        int min = Math.min(cache.get(beginWord), cache.get(newWord) + 2);
+                        cache.put(beginWord, min);
+                    }
+                    cache.put(word, cache.get(newWord) + 1);
+                }
+                arr[i] = orig;
+            }
+
+            if (cache.containsKey(word)) {
+                iter.remove();
+            }
+        }
+
+        if (notVisited.size() < setSize) {
+            helper2(beginWord, newSet, notVisited, letters, distance + 1, cache);
+        }
+    }
+
+    private boolean isTransformable(String w1, String w2) {
+        int diff = 0;
+        for (int i = 0; i < w1.length(); ++i) {
+            if (w1.charAt(i) == w2.charAt(i)) {
+                continue;
+            }
+            ++diff;
+            if (diff > 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private int helper5(String beginWord, Set<String> visited, Set<String> notVisited, char[] letters, int distance) {
         Set<String> newSet = new HashSet<>();
         Iterator iter = visited.iterator();
         while (iter.hasNext()) {
@@ -66,104 +175,7 @@ public class WordLadder {
             }
         }
 
-        return visited.size() == 0 ? -distance : 1 + helper(beginWord, newSet, notVisited, letters, distance + 1);
-    }
-
-    public int ladderLength1(String beginWord, String endWord, List<String> wordList) {
-        //System.out.println("beginword: " + beginWord + ", endword: " + endWord);
-        Set<String> set = new HashSet<>();
-        for (String w : wordList) {
-            set.add(w);
-        }
-
-        if (!set.contains(endWord)) {
-            return 0;
-        }
- 
-        Map<String, Integer> cache = new HashMap<>();
-        cache.put(endWord, 1);
-        int maxHops = wordList.size() + 2;
-        char[] letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-        int result = dfs(beginWord, set, cache, maxHops, letters);
-
-        return result == maxHops ? 0 : result;
-    }
-
-    private int dfs(String beginWord, Set<String> set, Map<String, Integer> cache, int maxHops, char[] letters) {
-        if (cache.containsKey(beginWord)) {
-            return cache.get(beginWord);
-        }
-
-        char[] arr = beginWord.toCharArray();
-        int min = maxHops;
-        for (int i = 0; i < arr.length; ++i) {
-            char orig = arr[i];
-            for (char c : letters) {
-                if (c == orig) {
-                    continue;
-                }
-                arr[i] = c;
-                String w = new String(arr);
-                if (!set.contains(w)) {
-                    continue;
-                }
-                set.remove(w);
-                min = Math.min(min, 1 + dfs(w, set, cache, maxHops, letters));
-                set.add(w);
-            }
-            arr[i] = orig;
-        }
-        //cache.put(beginWord, min);
-
-        return min;
-    }
-
-    public int ladderLength2(String beginWord, String endWord, List<String> wordList) {
-        if (!wordList.contains(endWord)) {
-            return 0;
-        } else if (isTransformable(beginWord, endWord)) {
-            return 2;
-        }
-
-        int result = dfs2(beginWord, endWord, wordList, new HashSet<String>(), new HashMap<String, Integer>());
-
-        return result == wordList.size() + 2 ? 0 : result;
-    }
-
-    private int dfs2(String beginWord, String endWord, List<String> wordList, Set<String> set, Map<String, Integer> cache) {
-        if (beginWord.equals(endWord)) {
-            return 1;
-        } else if (cache.containsKey(beginWord)) {
-            return cache.get(beginWord);
-        }
-
-        int min = wordList.size() + 2;
-        for (String word : wordList) {
-            if (set.contains(word) || !isTransformable(beginWord, word)) {
-                continue;
-            }
-            set.add(word);
-            min = Math.min(min, 1 + dfs2(word, endWord, wordList, set, cache));
-            set.remove(word);
-        }
-        cache.put(beginWord, min);
-
-        return min;
-    }
-
-    private boolean isTransformable(String w1, String w2) {
-        int diff = 0;
-        for (int i = 0; i < w1.length(); ++i) {
-            if (w1.charAt(i) == w2.charAt(i)) {
-                continue;
-            }
-            ++diff;
-            if (diff > 1) {
-                return false;
-            }
-        }
-
-        return true;
+        return visited.size() == 0 ? -distance : 1 + helper5(beginWord, newSet, notVisited, letters, distance + 1);
     }
 
     public static void main(String[] args) {
