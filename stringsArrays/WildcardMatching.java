a4b6efd9561a87e8c08b951d7279fc476abfddce
Pankaj Gupta <pankaj.gupta@spoken.com>
2019-12-05 16:44:57 -0800

    faster version: 76/100 percentile

--- stringsArrays/WildcardMatching.java
+++ stringsArrays/WildcardMatching.java
@@ -30,50 +30,31 @@ import java.util.Arrays;
 
 public class WildcardMatching {
     public boolean isMatch(String s, String p) {
-        int sl = s.length();
         int pl = p.length();
+        int sl = s.length();
+        boolean[] m = new boolean[sl + 1];
+        m[0] = true;
 
-        // create table
-        boolean[][] m = new boolean[pl + 1][sl + 1];
-        m[0][0] = true;
-        
-        // first col
-        for (int i = 1; i < m.length; ++i) {
-            if (p.charAt(i - 1) == '*') {
-                m[i][0] = m[i - 1][0];
-            }
-        }
-
-        // remaining cols
-        for (int row = 1; row < m.length; ++row) {
+        for (int row = 1; row <= pl; ++row) {
             char x = p.charAt(row - 1);
-            for (int col = 1; col < m[0].length; ++col) {
-                char y = s.charAt(col - 1);
-                if (x >= 'a' && y <= 'z') {
-                    m[row][col] = (x == y && m[row - 1][col - 1]);
-                } else if (x == '?') { // x = '?'
-                    m[row][col] = m[row - 1][col - 1];
+            boolean isAlpha = (x >= 'a' && x <= 'z');
+            boolean left = (x == '*' && m[0]);
+            for (int col = 1; col <= sl; ++col) {
+                boolean curr = false;
+                if (isAlpha) {
+                    curr = (x == s.charAt(col - 1) && m[col - 1]);
+                } else if (x == '?') {
+                    curr = m[col - 1];
                 } else { // x = '*'
-                    m[row][col] = m[row][col - 1] || m[row - 1][col - 1] || m[row - 1][col];
+                    curr = left || m[col];
                 }
+                m[col - 1] = left;
+                left = curr;
             }
+            m[sl] = left;
         }
-        
-        return m[pl][sl];
-    }
 
-    private void printMatrix(boolean[][] m) {
-        System.out.println("matrix:");
-        System.out.println("[");
-        for (int row = 0; row < m.length; ++row) {
-            System.out.print("  [ ");
-            for (int col = 0; col < m[0].length; ++col) {
-                char c = m[row][col] ? 'T' : 'F';
-                System.out.print(c + " ");
-            }
-            System.out.println("]");
-        }
-        System.out.println("]");
+        return m[sl];
     }
 
     public static void main(String[] args) {
