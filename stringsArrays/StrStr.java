3fe01a1990db45d39a2777e8961fed912e1929bb
Pankaj Gupta <pankaj.gupta@spoken.com>
2020-01-04 17:57:24 -0800

    accepted kmp version; 31/100 perc

--- stringsArrays/StrStr.java
+++ stringsArrays/StrStr.java
@@ -4,8 +4,56 @@
  * http://www.programcreek.com/2012/12/leetcode-implement-strstr-java/
  */
 
+import java.util.Arrays;
+
 public class StrStr {
     public int strStr(String haystack, String needle) {
+        int hl = haystack.length();
+        int nl = needle.length();
+        if (nl == 0) {
+            return 0;
+        } else if (hl < nl) {
+            return -1;
+        }
+        
+        int[] prefixArr = createPrefixArr(needle);
+        int i = 0, j = 0;
+        while (i < hl && j < nl) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                ++i;
+                ++j;
+            } else if (j == 0) {
+                ++i;
+            } else {
+                j = prefixArr[j - 1];
+            }
+        }
+
+        return j == nl ? i - j : -1;
+    }
+
+    private int[] createPrefixArr(String needle) {
+        int nl = needle.length();
+        int[] arr = new int[nl];
+        arr[0] = 0;
+        int i = 0, j = 1;
+        while (j < nl) {
+            if (needle.charAt(i) == needle.charAt(j)) {
+                arr[j] = i + 1;
+                ++i;
+                ++j;
+            } else if (i == 0) {
+                arr[j] = 0;
+                ++j;
+            } else {
+                i = arr[i - 1];
+            }
+        }
+
+        return arr;
+    }
+
+    public int strStr2(String haystack, String needle) {
         if (needle.length() == 0) {
             return 0;
         }
