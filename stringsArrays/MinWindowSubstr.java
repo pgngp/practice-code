424b212c046bd72bdfec6ee36967fed29d3a0449
Pankaj Gupta <pankaj.gupta@spoken.com>
2020-01-14 16:28:36 -0800

    wip

--- stringsArrays/MinWindowSubstr.java
+++ stringsArrays/MinWindowSubstr.java
@@ -18,6 +18,7 @@ public class MinWindowSubstr {
 
         Map<Character, Integer> countMap = new HashMap<>();
         Map<Character, Integer> map = new HashMap<>();
+        Deque<Integer> q = new ArrayDeque<>();
         for (int i = 0; i < tl; ++i) {
             char c = t.charAt(i);
             countMap.put(c, countMap.getOrDefault(c, 0) + 1);
@@ -26,7 +27,6 @@ public class MinWindowSubstr {
 
         int i = 0, j = 0, min = Integer.MAX_VALUE, count = 0;
         while (i <= j && j < sl) {
-            //System.out.println("i: " + i + ", j: " + j);
             while (j < sl && count < tl) {
                 char c = s.charAt(j);
                 if (map.containsKey(c)) {
@@ -34,40 +34,31 @@ public class MinWindowSubstr {
                         ++count;
                     }
                     map.put(c, map.get(c) + 1);
+                    q.addLast(j);
                 }
                 ++j;
             }
-            --j;
 
             if (count < tl) {
                 break;
-            } else if (min > j - i + 1) {
-                min = j - i + 1;
-                result = s.substring(i, j + 1);
+            } else if (min > j - i) {
+                min = j - i;
+                result = s.substring(i, j);
             }
-            //System.out.println("  j: " + j + ", count: " + count);
 
-            while (i <= j && count == tl) {
+            while (q.size() > 0 && count == tl) {
+                i = q.removeFirst();
                 char c = s.charAt(i);
-                if (map.containsKey(c)) {
-                    if (map.get(c) == countMap.get(c)) {
-                        --count;
-                    }
-                    map.put(c, map.get(c) - 1);
+                if (map.get(c) == countMap.get(c)) {
+                    --count;
                 }
+                map.put(c, map.get(c) - 1);
 
-                if (min > j - i + 1) {
-                    min = j - i + 1;
-                    result = s.substring(i, j + 1);
+                if (min > j - i) {
+                    min = j - i;
+                    result = s.substring(i, j);
                 }
-                ++i;
             }
-
-            while (i <= j && !countMap.containsKey(s.charAt(i))) {
-                ++i;
-            }
-            //System.out.println("  i: " + i + ", count: " + count);
-            ++j;
         }
 
         return result;
