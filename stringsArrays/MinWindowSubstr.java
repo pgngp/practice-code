a1f6fb3394f7455edca558df1d5ee77e076d2745
Pankaj Gupta <pankaj.gupta@spoken.com>
2020-01-15 10:09:34 -0800

    accepted version; 30/50 perc

--- stringsArrays/MinWindowSubstr.java
+++ stringsArrays/MinWindowSubstr.java
@@ -18,18 +18,19 @@ public class MinWindowSubstr {
 
         // refmap
         Map<Character, Integer> refMap = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < tl; ++i) {
             char c = t.charAt(i);
             refMap.put(c, refMap.getOrDefault(c, 0) + 1);
+            map.put(c, 0);
         }
 
         int i = 0, j = 0, min = Integer.MAX_VALUE, count = 0;
         Deque<Integer> q = new ArrayDeque<>();
-        Map<Character, Integer> map = new HashMap<>();
         while (j < sl) {
             char c = s.charAt(j);
             if (refMap.containsKey(c)) {
-                if (!map.containsKey(c) || map.get(c) < refMap.get(c)) {
+                if (map.get(c) < refMap.get(c)) {
                     ++count;
                 }
                 map.put(c, map.getOrDefault(c, 0) + 1);
@@ -45,14 +46,14 @@ public class MinWindowSubstr {
                 min = j - i;
                 result = s.substring(i, j);
             }
-            
+
             while (q.size() > 0 && count == tl) {
                 i = q.removeFirst();
                 char d = s.charAt(i);
                 if (map.get(d) <= refMap.get(d)) {
                     --count;
                 }
-                
+
                 if (min > j - i) {
                     min = j - i;
                     result = s.substring(i, j);
