b5aff390b680f4fec915a4bca7079e28e0278c31
Pankaj Gupta <pankaj.gupta@spoken.com>
2020-01-16 16:08:45 -0800

    accepted version; 5/6 perc

---
+++ stringsArrays/MinRotatedSortedArrII.java
@@ -0,0 +1,55 @@
+/**
+ * Find minimum in rotated sorted array II (103):
+ * Follow up for "Find Minimum in Rotated Sorted Array": What if duplicates are allowed?
+ * Would this affect the run-time complexity? How and why?
+ * http://www.programcreek.com/2014/03/leetcode-find-minimum-in-rotated-sorted-array-ii-java/
+ */
+
+import java.util.*;
+
+public class MinRotatedSortedArrII {
+    public int findMin(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    private int helper(int[] nums, int s, int e) {
+        if (s > e) {
+            return Integer.MAX_VALUE;
+        } else if (e - s == 0 || nums[s] < nums[e]) {
+            return nums[s];
+        }
+
+        int min = nums[s];
+        while (s <= e) {
+            int m = (s + e) / 2;
+            if (m > 0 && nums[m - 1] > nums[m]) { // min is mid
+                min = nums[m];
+                break;
+            } else if (nums[s] < nums[e]) {
+                min = nums[s];
+                break;
+            } else if (nums[m] < nums[e] || nums[s] > nums[m]) { // min is on left
+                e = m - 1;
+            } else if (nums[m] > nums[e] || nums[s] < nums[m]) { // min is on right
+                s = m + 1;
+            } else { // min can be on either side
+                min = Math.min(helper(nums, s, m - 1), helper(nums, m + 1, e));
+                break;
+            }
+        }
+
+        return min;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[args.length];
+        for (int i = 0; i < nums.length; ++i) {
+            nums[i] = Integer.parseInt(args[i]);
+        }
+        System.out.println("input: " + Arrays.toString(nums));
+
+        MinRotatedSortedArrII mrsa = new MinRotatedSortedArrII();
+        int result = mrsa.findMin(nums);
+        System.out.println("result: " + result);
+    }
+}
