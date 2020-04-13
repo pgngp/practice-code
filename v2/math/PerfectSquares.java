45dbe593bfa5c859d2fefa8dbb8dcaba10888a63
Pankaj Gupta <pankaj.gupta@spoken.com>
2020-04-12 18:18:13 -0700

    accepted version; 94/13 perc

--- v2/math/PerfectSquares.java
+++ v2/math/PerfectSquares.java
@@ -12,6 +12,51 @@ public class PerfectSquares {
             return 1;
         }
 
+        // list of perfect squares
+        Set<Integer> set = new HashSet<>();
+        int ps = 0;
+        for (int i = 1; i < n; i += 2) {
+            ps += i;
+            if (ps > n) {
+                break;
+            }
+            set.add(ps);
+        }
+
+        // bfs
+        Deque<Integer> q = new ArrayDeque<>();
+        q.addLast(Integer.MAX_VALUE);
+        q.addLast(n);
+        int steps = 0;
+        while (q.size() > 0) {
+            int x = q.removeFirst();
+            if (x == Integer.MAX_VALUE) {
+                ++steps;
+                q.add(Integer.MAX_VALUE);
+                continue;
+            }
+
+            Iterator<Integer> it = set.iterator();
+            while (it.hasNext()) {
+                int rem = x - it.next();
+                if (set.contains(rem)) {
+                    return steps + 1;
+                } else if (rem < 0) {
+                    continue;
+                }
+                q.addLast(rem);
+            }
+        }
+
+        return n;
+    }
+
+    public int numSquares2(int n) {
+        double sqrt = Math.sqrt(n);
+        if (sqrt == Math.floor(sqrt)) {
+            return 1;
+        }
+
         Map<Integer, Integer> map = new HashMap<>();
         List<Integer> list = new ArrayList<>();
         int ps = 0;
@@ -37,8 +82,8 @@ public class PerfectSquares {
         }
 
         int min = Integer.MAX_VALUE;
-        for (int i = 0; i < list.size(); ++i) {
-            min = Math.min(min, 1 + getCount(list, x - list.get(i), map));
+        for (int ps : list) {
+            min = Math.min(min, 1 + getCount(list, x - ps, map));
         }
         map.put(x, min);
 
