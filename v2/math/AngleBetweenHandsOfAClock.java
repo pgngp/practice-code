bf264c14173df2a5be50cdee1043fb4913d96c88
Pankaj Gupta <pankaj.gupta@spoken.com>
2020-04-13 20:35:19 -0700

    accepted version; 100/100 perc

---
+++ v2/math/AngleBetweenHandsOfAClock.java
@@ -0,0 +1,23 @@
+/**
+ * Angle between Hands of a Clock:
+ * https://leetcode.com/problems/angle-between-hands-of-a-clock/
+ */
+
+import java.util.*;
+
+public class AngleBetweenHandsOfAClock {
+    public double angleClock(int hour, int minutes) {
+        hour %= 12;
+        double hourAngle = ((hour * 5) + ((double) minutes / 12)) * 6;
+        double minuteAngle = ((double) minutes) * 6;
+        double result = hourAngle > minuteAngle ? hourAngle - minuteAngle : minuteAngle - hourAngle;
+
+        return result < 180 ? result : 360 - result;
+    }
+
+    public static void main(String[] args) {
+        AngleBetweenHandsOfAClock a = new AngleBetweenHandsOfAClock();
+        double result = a.angleClock(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        System.out.println("result: " + result);
+    }
+}
