4b54265594df88379369f8deaeb6d59d8cfd3ff0
Pankaj Gupta <pankajvedg@gmail.com>
2020-05-08 15:18:27 -0700

    changed class name

--- v2/WordSearch.java
+++ v2/WordSearch.java
@@ -1,4 +1,4 @@
-class Solution {
+class WordSearch {
     public boolean exist(char[][] board, String word) {
         for (int row = 0; row < board.length; ++row) {
             for (int col = 0; col < board[0].length; ++col) {
