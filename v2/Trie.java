b44a8ec3f0ae917dd3f8a9576b29bbb9b7d56b34
Pankaj Gupta <pankajvedg@gmail.com>
2020-05-14 10:41:47 -0700

    initial commit

---
+++ v2/Trie.java
@@ -0,0 +1,67 @@
+class Trie {
+    private final TrieNode root;
+
+    /** Initialize your data structure here. */
+    public Trie() {
+        root = new TrieNode();
+    }
+    
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); ++i) {
+            char c = word.charAt(i);
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
+            }
+            node = node.children[c - 'a'];
+        }
+        node.word = word;
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); ++i) {
+            char c = word.charAt(i);
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        
+        return node.word == null ? false : true;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (int i = 0; i < prefix.length(); ++i) {
+            char c = prefix.charAt(i);
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        
+        return true;
+    }
+    
+    public class TrieNode {
+        private TrieNode[] children;
+        private String word;
+        
+        public TrieNode() {
+            children = new TrieNode[26];
+            word = null;
+        }
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
