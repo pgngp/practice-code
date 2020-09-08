827a9b006c777b724fe32ac1cbb55cbc174414d8
Pankaj Gupta <pankajvedg@gmail.com>
2020-09-08 14:17:58 -0700

    Create AllElementsIn2BST.java

---
+++ v2/AllElementsIn2BST.java
@@ -0,0 +1,59 @@
+/* https://leetcode.com/problems/all-elements-in-two-binary-search-trees/ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class AllElementsIn2BST {
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> list1 = new ArrayList<>();
+        inorder(root1, list1);
+        
+        List<Integer> list2 = new ArrayList();
+        inorder(root2, list2);
+        
+        List<Integer> result = new ArrayList<>();
+        int i = 0, j = 0;
+        while (i < list1.size() && j < list2.size()) {
+            if (list1.get(i) <= list2.get(j)) {
+                result.add(list1.get(i));
+                i++;
+            } else {
+                result.add(list2.get(j));
+                j++;
+            }
+        }
+                           
+        while (i < list1.size()) {
+            result.add(list1.get(i));
+            i++;
+        }
+                           
+        while (j < list2.size()) {
+            result.add(list2.get(j));
+            j++;
+        }
+                           
+        return result;
+    }
+    
+    private void inorder(TreeNode node, List<Integer> list) {
+        if (node == null) {
+            return;
+        }
+        
+        inorder(node.left, list);
+        list.add(node.val);
+        inorder(node.right, list);
+    }
+}
